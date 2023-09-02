8e95c6a7aaf07276a594b933b1c5f4a109b8cb56
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-02 13:48:58 +0300

    Create obstacle configuration

---
+++ Assets/Scripts/Data/Static/Configuration/Obstacle/ObstacleConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace BlockBreaker.Data.Static.Configuration.Obstacle
+{
+    [CreateAssetMenu(fileName = nameof(ObstacleConfig), menuName = "Configuration/Obstacle/BlockObstacle")]
+    public class ObstacleConfig : ScriptableObject
+    {
+        [field: SerializeField, Min(default)] public int HealthPoints { get; private set; }
+        [field: SerializeField, Min(default)] public Vector3 Radius { get; private set; }
+    }
+}
