59367f30e988f395a6b3bff23ac20ba435c61eba
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-03 12:18:30 +0300

    Create player input handler

---
+++ Assets/Scripts/Features/Player/PlayerInputHandler.cs
@@ -0,0 +1,33 @@
+﻿using BlockBreaker.Infrastructure.Services;
+using BlockBreaker.Infrastructure.Services.Input;
+using UnityEngine;
+
+namespace BlockBreaker.Features.Player
+{
+    public class PlayerInputHandler : IActiveService
+    {
+        private readonly IPlayerTouchInputService _touchInputService;
+
+        public PlayerInputHandler(IPlayerTouchInputService touchInputService) => _touchInputService = touchInputService;
+
+        ~PlayerInputHandler() => Disable();
+
+        public void Enable()
+        {
+            _touchInputService.OnTouchBegan += OnTouchBegan;
+            _touchInputService.OnTouchEnded += OnTouchEnded;
+            _touchInputService.OnTouchHold += OnTouchHold;
+        }
+
+        public void Disable()
+        {
+            _touchInputService.OnTouchBegan -= OnTouchBegan;
+            _touchInputService.OnTouchEnded -= OnTouchEnded;
+            _touchInputService.OnTouchHold -= OnTouchHold;
+        }
+
+        private void OnTouchBegan() => Debug.Log("Touch began");
+        private void OnTouchEnded() => Debug.Log("Touch ended");
+        private void OnTouchHold() => Debug.Log("Touch hold");
+    }
+}
