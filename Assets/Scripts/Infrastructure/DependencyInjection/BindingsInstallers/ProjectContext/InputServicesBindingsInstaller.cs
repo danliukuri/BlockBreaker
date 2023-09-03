4bde0588bca199dd83e6b1a52057172f9f366e60
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-03 12:20:37 +0300

    Enable player input handling in setup gameplay state

---
+++ Assets/Scripts/Infrastructure/DependencyInjection/BindingsInstallers/ProjectContext/InputServicesBindingsInstaller.cs
@@ -0,0 +1,12 @@
+﻿using BlockBreaker.Infrastructure.Services.Input;
+using Zenject;
+
+namespace BlockBreaker.Infrastructure.DependencyInjection.BindingsInstallers.ProjectContext
+{
+    public class InputServicesBindingsInstaller : MonoInstaller
+    {
+        public override void InstallBindings() => BindPlayerTouchInputService();
+
+        private void BindPlayerTouchInputService() => Container.BindInterfacesTo<PlayerTouchInputService>().AsSingle();
+    }
+}
