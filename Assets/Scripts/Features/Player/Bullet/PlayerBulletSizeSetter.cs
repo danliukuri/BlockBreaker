eaed792fc9854522b85264d71b4bc2f041a119a9
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-03 19:49:01 +0300

    Create bullet size setter

---
+++ Assets/Scripts/Features/Player/Bullet/PlayerBulletSizeSetter.cs
@@ -0,0 +1,24 @@
+﻿using BlockBreaker.Data.Dynamic.Player;
+using UnityEngine;
+
+namespace BlockBreaker.Features.Player.Bullet
+{
+    public class PlayerBulletSizeSetter
+    {
+        public void Set(PlayerBulletData bullet, float size)
+        {
+            bullet.Size = size;
+            bullet.Radius = size / 2f;
+            bullet.Transform.localScale = Vector3.one * bullet.Size;
+            
+            CorrectPosition(bullet);
+        }
+
+        private void CorrectPosition(PlayerBulletData bullet)
+        {
+            Vector3 initialPosition = bullet.InitialPosition;
+            bullet.Transform.localPosition =
+                new Vector3(initialPosition.x, initialPosition.y + bullet.Radius, initialPosition.z);
+        }
+    }
+}
