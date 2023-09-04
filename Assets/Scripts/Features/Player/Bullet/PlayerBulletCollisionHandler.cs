e6aabd3adb120153ed914bf8d2f196003c07c2e1
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-04 12:42:32 +0300

    Make bullets explode on collision with obstacles

---
+++ Assets/Scripts/Features/Player/Bullet/PlayerBulletCollisionHandler.cs
@@ -0,0 +1,19 @@
+﻿using BlockBreaker.Features.Obstacle;
+using UnityEngine;
+
+namespace BlockBreaker.Features.Player.Bullet
+{
+    [RequireComponent(typeof(PlayerBulletDataProvider))]
+    public class PlayerBulletCollisionHandler : MonoBehaviour
+    {
+        private PlayerBulletDataProvider _bulletProvider;
+
+        private void Awake() => _bulletProvider = GetComponent<PlayerBulletDataProvider>();
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out ObstacleDataProvider obstacleProvider))
+                _bulletProvider.Data.Exploder.Explode(_bulletProvider);
+        }
+    }
+}
