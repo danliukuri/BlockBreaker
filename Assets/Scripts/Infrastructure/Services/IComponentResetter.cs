815ddbc6b860e67593bc76ad22123d026a24292d
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-04 21:24:43 +0300

    Create bullet resetter

---
+++ Assets/Scripts/Infrastructure/Services/IComponentResetter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace BlockBreaker.Infrastructure.Services
+{
+    public interface IComponentResetter<in TComponent> where TComponent : Component
+    {
+        void Reset(TComponent component);
+    }
+}
