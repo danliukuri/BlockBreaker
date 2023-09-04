2a16c26eee8301c776fe2a2a80b4282be87acdeb
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-04 22:10:04 +0300

    Move player to its goal on victory

--- Assets/Scripts/Architecture/GameStates/Gameplay/VictoryGameplayState.cs
+++ Assets/Scripts/Architecture/GameStates/Gameplay/VictoryGameplayState.cs
@@ -1,9 +1,14 @@
-﻿using BlockBreaker.Utilities.Patterns.State;
+﻿using BlockBreaker.Features.Player;
+using BlockBreaker.Utilities.Patterns.State;
 
 namespace BlockBreaker.Architecture.GameStates.Gameplay
 {
     public class VictoryGameplayState : IEnterableState
     {
-        public void Enter() => UnityEngine.Debug.Log("Victory");
+        private readonly PlayerMover _playerMover;
+
+        public VictoryGameplayState(PlayerMover playerMover) => _playerMover = playerMover;
+        
+        public void Enter() => _playerMover.Move();
     }
 }
