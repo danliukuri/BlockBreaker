1174f6b6629a18b31c78314ae85dc66788a7e0fc
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-05 16:10:50 +0300

    Create UI text for level end (#18)
    * Add TextMeshPro plugin to gitignore
    
    * Add Caesar Dressing font
    
    * Create victory text prefab
    
    * Create defeat text prefab
    
    * Create victory text factory and pool configuration
    
    * Create defeat text factory and pool configuration
    
    * Show defeat text on entering to defeat gameplay state
    
    * Show victory text on entering to victory gameplay state
    
    * Create defeat text appearing animation
    
    * Create victory text appearing animation
    
    * Disable canvas

--- Assets/Scripts/Architecture/GameStates/Gameplay/VictoryGameplayState.cs
+++ Assets/Scripts/Architecture/GameStates/Gameplay/VictoryGameplayState.cs
@@ -1,14 +1,25 @@
 ﻿using BlockBreaker.Features.Player;
+using BlockBreaker.Features.UI;
 using BlockBreaker.Utilities.Patterns.State;
+using UnityEngine.Pool;
 
 namespace BlockBreaker.Architecture.GameStates.Gameplay
 {
     public class VictoryGameplayState : IEnterableState
     {
         private readonly PlayerMover _playerMover;
+        private readonly IObjectPool<VictoryTextMarker> _victoryTexts;
 
-        public VictoryGameplayState(PlayerMover playerMover) => _playerMover = playerMover;
-        
-        public void Enter() => _playerMover.Move();
+        public VictoryGameplayState(PlayerMover playerMover, IObjectPool<VictoryTextMarker> victoryTexts)
+        {
+            _playerMover = playerMover;
+            _victoryTexts = victoryTexts;
+        }
+
+        public void Enter()
+        {
+            _playerMover.Move();
+            _victoryTexts.Get();
+        }
     }
 }
---
+++ Assets/Scripts/Infrastructure/DependencyInjection/BindingsInstallers/SceneContext/UIBindingsInstaller.cs
@@ -0,0 +1,94 @@
+﻿using BlockBreaker.Data.Static.Configuration;
+using BlockBreaker.Features.UI;
+using BlockBreaker.Infrastructure.Factories.Components;
+using UnityEngine;
+using UnityEngine.Pool;
+using Zenject;
+
+namespace BlockBreaker.Infrastructure.DependencyInjection.BindingsInstallers.SceneContext
+{
+    public class UIBindingsInstaller : MonoInstaller
+    {
+        [SerializeField] private Transform uiRoot;
+        [SerializeField] private GameObject canvas;
+
+        [SerializeField] private FactoryConfig defeatTextFactoryConfig;
+        [SerializeField] private PoolConfig defeatTextPoolConfig;
+
+        [SerializeField] private FactoryConfig victoryTextFactoryConfig;
+        [SerializeField] private PoolConfig victoryTextPoolConfig;
+
+        public override void InstallBindings()
+        {
+            BindConfigurator();
+
+            BindDefeatTextFactory();
+            BindDefeatTextObjectPool();
+            
+            BindVictoryTextFactory();
+            BindVictoryTextObjectPool();
+        }
+
+        private void BindConfigurator()
+        {
+            Container
+                .BindInterfacesTo<UIConfigurator>()
+                .AsSingle()
+                .WithArguments(canvas, uiRoot)
+                .WhenInjectedInto(typeof(IComponentFactory<DefeatTextMarker>),
+                    typeof(IComponentFactory<VictoryTextMarker>));
+        }
+
+        private void BindDefeatTextFactory()
+        {
+            Container
+                .BindInterfacesTo<FactoryConfig>()
+                .FromScriptableObject(defeatTextFactoryConfig)
+                .AsCached()
+                .WhenInjectedInto<ComponentFactory<DefeatTextMarker>>();
+
+            Container.BindInterfacesTo<ComponentFactory<DefeatTextMarker>>().AsCached();
+        }
+
+        private void BindDefeatTextObjectPool()
+        {
+            Container
+                .BindInterfacesTo<PoolConfig>()
+                .FromScriptableObject(defeatTextPoolConfig)
+                .AsCached()
+                .WhenInjectedInto<ComponentPoolFactory<DefeatTextMarker>>();
+
+            Container
+                .Bind<IObjectPool<DefeatTextMarker>>()
+                .To<ObjectPool<DefeatTextMarker>>()
+                .FromFactory<ComponentPoolFactory<DefeatTextMarker>>()
+                .AsCached();
+        }
+
+        private void BindVictoryTextFactory()
+        {
+            Container
+                .BindInterfacesTo<FactoryConfig>()
+                .FromScriptableObject(victoryTextFactoryConfig)
+                .AsCached()
+                .WhenInjectedInto<ComponentFactory<VictoryTextMarker>>();
+
+            Container.BindInterfacesTo<ComponentFactory<VictoryTextMarker>>().AsCached();
+        }
+
+        private void BindVictoryTextObjectPool()
+        {
+            Container
+                .BindInterfacesTo<PoolConfig>()
+                .FromScriptableObject(victoryTextPoolConfig)
+                .AsCached()
+                .WhenInjectedInto<ComponentPoolFactory<VictoryTextMarker>>();
+
+            Container
+                .Bind<IObjectPool<VictoryTextMarker>>()
+                .To<ObjectPool<VictoryTextMarker>>()
+                .FromFactory<ComponentPoolFactory<VictoryTextMarker>>()
+                .AsCached();
+        }
+    }
+}
