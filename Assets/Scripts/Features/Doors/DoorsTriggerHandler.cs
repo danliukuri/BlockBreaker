41ad351af77d47d50277a32490a281e0046a7097
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-05 13:57:25 +0300

    Activate doors opening animation when player enters trigger zone

---
+++ Assets/Scripts/Features/Doors/DoorsTriggerHandler.cs
@@ -0,0 +1,18 @@
+﻿using BlockBreaker.Features.Player;
+using UnityEngine;
+
+namespace BlockBreaker.Features.Doors
+{
+    public class DoorsTriggerHandler : MonoBehaviour
+    {
+        private DoorsAnimationActivator _doorsAnimationActivator;
+
+        private void Awake() => _doorsAnimationActivator = GetComponentInParent<DoorsAnimationActivator>();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out PlayerDataProvider playerDataProvider))
+                _doorsAnimationActivator.Open();
+        }
+    }
+}
