c14130051abba02fc05918f6de0cb3fb40652106
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-05 13:49:41 +0300

    Create doors animation activator

---
+++ Assets/Scripts/Features/Doors/DoorsAnimationActivator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BlockBreaker.Features.Doors
+{
+    [RequireComponent(typeof(Animator))]
+    public class DoorsAnimationActivator : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Awake() => _animator = GetComponent<Animator>();
+
+        public void Open() => _animator.SetTrigger(nameof(Open));
+    }
+}
